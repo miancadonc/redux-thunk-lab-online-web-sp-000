4a9fd6b3be1f5bb2f4a9ae7960ce8ca3b904c137
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-27 22:59:02 -0500

    commit tree f305b4cb39e8996d5c1b4c5efeafe1f1db6ab313

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,13 @@
 // write and export your action creator function here
+
+export const fetchCats = () => {
+    return (dispatch) => {
+        dispatch({type: "LOADING_CATS"})
+
+        fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+        .then(resp => { return resp.json()})
+        .then(json => {
+        dispatch({type: "ADD_CATS", cats: json.images})
+        })
+    }
+}
--- src/reducers/catsReducer.js
+++ src/reducers/catsReducer.js
@@ -1,1 +1,29 @@
 // add the catsReducer
+
+const catsReducer = (state = {
+    cats: [],
+    loading: false
+}, action) => {
+    switch(action.type){
+        case "LOADING_CATS":
+            return {
+                ...state,
+                cats: [...state.cats],
+                loading: true
+            }
+
+        case "ADD_CATS":
+            return {
+                ...state,
+                cats: action.cats,
+                loading: false
+            }
+
+        default:
+            return state
+    }
+}
+
+export default catsReducer
+
+// Is the second copying of the cats key in "LOADING_CATS" meant to deep copy or something like that?
